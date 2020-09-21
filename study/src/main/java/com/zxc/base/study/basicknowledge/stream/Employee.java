38af05862ab4c61d2b15d471274e37a37d95ce63
zxc <zxc@12.com>
2020-09-21 18:00:01 +0800

    stream study

--- study/src/main/java/com/zxc/base/study/basicknowledge/stream/Employee.java
+++ study/src/main/java/com/zxc/base/study/basicknowledge/stream/Employee.java
@@ -2,12 +2,14 @@ package com.zxc.base.study.basicknowledge.stream;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author zxc
  * @date 2020/9/18 11:50
  */
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class Employee {
     private String name;
--- study/src/main/java/com/zxc/base/study/basicknowledge/stream/StreamTest.java
+++ study/src/main/java/com/zxc/base/study/basicknowledge/stream/StreamTest.java
@@ -1,5 +1,6 @@
 package com.zxc.base.study.basicknowledge.stream;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -7,11 +8,14 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author zxc
  * @date 2020/9/18 14:09
  */
+@Slf4j
 public class StreamTest {
 
     @Autowired
@@ -71,5 +75,53 @@ public class StreamTest {
         //min---返回流中最小值
         Optional<Employee> min = employees.stream().min(Comparator.comparing(Employee::getAge));
         System.out.println("min: "+min.get());
+    }
+
+    @Test
+    public void test2(){
+        long l = System.currentTimeMillis();
+//stream()
+        employees.stream().map(e -> {
+            Employee employee = new Employee();
+            employee.setAge(e.getAge()+1);
+            employee.setName(e.getName());
+            employee.setSalary(e.getSalary());
+            employee.setStatus(e.getStatus());
+            return employee;
+        }).collect(Collectors.toList()).forEach(e -> System.out.println(e));
+        System.out.println(l-System.currentTimeMillis());
+        System.out.println("111111111111111");
+        long l1 = System.currentTimeMillis();
+//parallelStream()
+        employees.parallelStream().map(e -> {
+            Employee employee = new Employee();
+            employee.setAge(e.getAge()+1);
+            employee.setName(e.getName());
+            employee.setSalary(e.getSalary());
+            employee.setStatus(e.getStatus());
+            return employee;
+        }).collect(Collectors.toList()).forEach(employee1 -> System.out.println(employee1));
+        System.out.println(l1-System.currentTimeMillis());
+
+//flatMap(): 能够将一个二维的集合映射成一个一维的集合
+        List<String> list = Arrays.asList("1 2", "3 4", "5 6");
+        list.stream().flatMap(e -> {
+            log.info(e.toString());
+            Stream<String> stream = Arrays.stream(e.split(" "));
+            log.info(stream.toString());
+            return stream;}).forEach(System.out::println);
+        Stream<Stream<String>> streamStream = list.stream().map(item -> Arrays.stream(item.split(" ")));
+        list.stream().map(item -> Arrays.stream(item.split(" "))).forEach(n -> n.forEach(System.out::println));
+
+//reduce()方法: 用于将流中的所有值合成一个
+        List<Integer> numbers = Arrays.asList(-1, -2, 0, -1, 4, 5, 1);
+        Integer total = numbers.stream().reduce((t, n) -> t + n).get();
+        System.out.println("Total: " + total);
+
+//summaryStatistics(): 并不是Stream接口的方法，而是Stream API采用mapToInt()、mapToLong()、mapToDouble()三个方法分别生成IntStream 、
+// LongStream 、DoubleStream 三个接口类型的对象，这个方法的参数分别为3个函数式接口ToIntFunction、ToLongFunction、ToDoubleFunction，
+// 使用时可以用lambda表达式计算返回对应的int、long、double类型即可
+
+
     }
 }
